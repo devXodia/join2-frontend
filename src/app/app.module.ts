4ed3595e1db2348d571582891cdabcce3d652aa8
Alen Alduk <youstylez@gmail.com>
2023-12-10 15:14:39 +0100

    login page visuals done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  checkbox: boolean = true;
+  email: string = '';
+  pwd: string = '';
 
+  ngOnInit() {
+    this.getLocalStorageInfo();
+  }
+
+  changeCheckbox() {
+    this.checkbox = !this.checkbox;
+
+    if (this.checkbox) {
+      localStorage.setItem('email', this.email);
+      localStorage.setItem('password', this.pwd);
+    }
+  }
+
+  getLocalStorageInfo() {
+    if (typeof localStorage !== 'undefined') {
+      let email = localStorage.getItem('email');
+      let password = localStorage.getItem('password');
+      if (email && password) {
+        this.email = email;
+        this.pwd = password;
+      }
+    } else {
+      console.error('localStorage is not available in this environment.');
+    }
+  }
 }
