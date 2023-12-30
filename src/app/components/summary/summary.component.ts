e2eb7df212f2770d4c892eb8f03d2f35124e115c
devXodia <youstylez@gmail.com>
2023-12-30 14:07:36 +0100

    working on dashboard

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-summary',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './summary.component.scss'
 })
 export class SummaryComponent {
+
+  
 
 }
