2d0c57169da9f0ee1d17d52bba8a4a4ef3cabecd
devXodia <youstylez@gmail.com>
2024-01-03 15:20:35 +0100

    changed file structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,36 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { SummaryComponent } from './components/summary/summary.component';
-import { BoardComponent } from './components/board/board.component';
+import { SummaryComponent } from './components/dashboard/summary/summary.component';
+import { BoardComponent } from './components/dashboard/board/board.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { AddTaskComponent } from './components/dashboard/add-task/add-task.component';
+import { ContactsComponent } from './components/dashboard/contacts/contacts.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: SignUpComponent },
-  { path: 'summary', component: SummaryComponent },
   { path: 'board', component: BoardComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+        {
+           path: 'summary',
+           component: SummaryComponent
+        },
+        {
+           path: 'board',
+           component: BoardComponent
+        },
+        {
+          path: 'add-task',
+          component: AddTaskComponent
+       },
+       {
+        path: 'contacts',
+        component: ContactsComponent
+     }]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { SummaryComponent } from './components/summary/summary.component';
-import { BoardComponent } from './components/board/board.component';
+
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { AddTaskComponent } from './components/dashboard/add-task/add-task.component';
+import { ContactsComponent } from './components/dashboard/contacts/contacts.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignUpComponent, SummaryComponent, BoardComponent],
+  declarations: [AppComponent, LoginComponent, SignUpComponent, DashboardComponent, AddTaskComponent, ContactsComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
