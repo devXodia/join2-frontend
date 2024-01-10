8dc9e4e8537f80b3c591cb18b61437737f033cf4
devXodia <youstylez@gmail.com>
2024-01-10 12:04:47 +0100

    working on dashboard

--- src/app/components/dashboard/summary/summary.component.ts
+++ src/app/components/dashboard/summary/summary.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-summary',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class SummaryComponent {
 
+ 
+  //Connect Summary to Backend
 }
