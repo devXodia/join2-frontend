841a278a6ec8ca5bc208353335a55d21b764627f
devXodia <youstylez@gmail.com>
2024-02-25 13:39:26 +0100

    working on task creatiojn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,36 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-
 import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
 import { AddTaskComponent } from './components/dashboard/add-task/add-task.component';
 import { ContactsComponent } from './components/dashboard/contacts/contacts.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignUpComponent, DashboardComponent, AddTaskComponent, ContactsComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [provideClientHydration()],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    DashboardComponent,
+    AddTaskComponent,
+    ContactsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormField,
+    MatLabel,
+    MatError,
+  ],
+  providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/dashboard/add-task/add-task.component.ts
+++ src/app/components/dashboard/add-task/add-task.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.scss'
+  styleUrl: './add-task.component.scss',
 })
 export class AddTaskComponent {
+  todayDate: Date = new Date();
+  picker: Date = new Date();
+  error: boolean = false;
 
+  constructor(private dateAdapter: DateAdapter<Date>) {
+    this.dateAdapter.setLocale('de-DE');
+  }
+
+  checkDate(event: any) {
+    // function to invalid date ;
+  }
 }
