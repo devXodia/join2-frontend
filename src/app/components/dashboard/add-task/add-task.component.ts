f792f8565f5b7a6d6ddd70a9cb9454ba68a6e227
devXodia <youstylez@gmail.com>
2024-02-26 14:32:11 +0100

    working on task creation

--- src/app/components/dashboard/add-task/add-task.component.ts
+++ src/app/components/dashboard/add-task/add-task.component.ts
@@ -10,12 +10,42 @@ export class AddTaskComponent {
   todayDate: Date = new Date();
   picker: Date = new Date();
   error: boolean = false;
+  urgent: boolean = false;
+  medium: boolean = false;
+  low: boolean = false;
 
   constructor(private dateAdapter: DateAdapter<Date>) {
     this.dateAdapter.setLocale('de-DE');
   }
 
   checkDate(event: any) {
-    // function to invalid date ;
+    // function to handle errors on invalid date
+  }
+
+  changeUrgentPrio() {
+    this.urgent = !this.urgent;
+
+    if (this.urgent) {
+      this.medium = false;
+      this.low = false;
+    }
+  }
+
+  changeMediumPrio() {
+    this.medium = !this.medium;
+
+    if (this.medium) {
+      this.urgent = false;
+      this.low = false;
+    }
+  }
+
+  changeLowPrio() {
+    this.low = !this.low;
+
+    if (this.low) {
+      this.urgent = false;
+      this.medium = false;
+    }
   }
 }
