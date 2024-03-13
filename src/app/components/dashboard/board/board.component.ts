e2175348dc713c62a219984c2c41342fd81d9832
devXodia <youstylez@gmail.com>
2024-03-13 16:01:36 +0100

    working on board

--- src/app/components/dashboard/board/board.component.ts
+++ src/app/components/dashboard/board/board.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CdkDrag } from '@angular/cdk/drag-drop';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { Task } from '../../../interfaces/task.interface';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -8,4 +9,23 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
   imports: [MatProgressBarModule, CdkDrag],
   standalone: true,
 })
-export class BoardComponent {}
+export class BoardComponent {
+  todo: Task[] = [];
+  progress: Task[] = [];
+  feedback: Task[] = [];
+  done: Task[] = [];
+
+  ngOnInit() {
+    const newTask: Task = {
+      title: 'Test task',
+      description: 'Hello description',
+      duedate: new Date(),
+      prio: 'high',
+      assignedTo: ['Harry Potter'],
+      category: 'Media',
+      subtasks: [],
+    };
+
+    this.todo.push(newTask);
+  }
+}
